b1d51cd4ae6dfed77195b3b5e980edc877ea9d9a
David Motoche <davidmotoche1@gmail.com>
2025-12-25 10:12:33 -0500

    Inicializando el repositorio de git

---
+++ Juego de Poker.py
@@ -0,0 +1,9 @@
+# Creando un Juego de Poker
+# Que necesita el juego?
+'''
+Bueno el poker tiene varias versiones en si mismo
+primero 
+hay que definir las barajas 
+- Son 4 palos: Diamante, Corazon, Tipcas, Trebol
+
+'''
